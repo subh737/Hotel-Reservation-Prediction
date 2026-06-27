bc8fa563e6c7f2152f6c02b4065db09a4b0549c3
Subham Pradhan <subhampradhan737@gmail.com>
2026-06-27 10:52:17 +0530

    Errors fixed

--- src/custom_exception.py
+++ src/custom_exception.py
@@ -8,7 +8,7 @@ class CustomException(Exception):
 
     @staticmethod
     def get_detailed_error_message(error_message, error_details: sys):
-        _, _, exc_tb = error_details.exc_info()
+        _, _, exc_tb = traceback.sys.exc_info()
         file_name = exc_tb.tb_frame.f_code.co_filename
         line_number = exc_tb.tb_lineno
         detailed_error_message = f"Error occurred in script: [{file_name}] at line number: [{line_number}] with error message: [{error_message}]"
